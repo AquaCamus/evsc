f5ae07cbe463ac100f173ef8b24bc3a2c02bfb64
Gabriel Viana <aqua.camus@hotmail.com>
2020-02-03 02:25:52 -0300

    Nada demais

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,31 @@
 package app;
 
-public class Program {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/CadastroEmpresa.fxml"));
+			Pane pane = loader.load();
+			
+			Scene mainScene = new Scene(pane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-public class Main extends Application {
+public class MainViewController extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
